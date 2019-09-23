c5843437354d46022107117f34c90005fc35db72
artseven <yabkevich.artsiom.09@gmail.com>
2019-09-23 17:01:31 -0400

    rendering a meals list

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,31 +1,48 @@
 import React from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
 
-import { CATEGORIES, MEALS } from '../data/dummy-data';
-import Colors from '../constants/Colors'
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import Colors from "../constants/Colors";
+import MealItem from "../components/MealItem";
 
 const CategoryMealsScreen = props => {
-	const renderMealItem = itemData => {
-		return (<View><Text>{itemData.item.title}</Text></View>);
-	}
-	const catId = props.navigation.getParam('categoryId');
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+			title={itemData.item.title}
+			image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+        onSelectMeal={() => {}}
+      />
+    );
+  };
+  const catId = props.navigation.getParam("categoryId");
 
-	const displayedMeals = MEALS.filter(meal => meal.categoryIds.indexOf(catId) >= 0);
-	return (
-		<View style={styles.screen}>
-			<FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem}/>
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%", padding: 10 }}
+      />
     </View>
   );
 };
 
-CategoryMealsScreen.navigationOptions = (navigationData) => {
-	const catId = navigationData.navigation.getParam('categoryId');
+CategoryMealsScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam("categoryId");
 
-	const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
-	return {
-		headerTitle: selectedCategory.title
-	};
+  return {
+    headerTitle: selectedCategory.title
+  };
 };
 
 const styles = StyleSheet.create({
