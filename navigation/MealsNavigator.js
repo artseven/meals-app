b886e2755f9d7accd0fc797f97d9d97bc8dd4f86
artseven <yabkevich.artsiom.09@gmail.com>
2019-10-17 11:00:46 -0400

    additional styling

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -69,7 +69,10 @@ const MealsFavTabNavigator =
   Platform.OS === "android"
     ? createMaterialBottomTabNavigator(tabScreenConfig, {
       activeTintColor: 'white',
-      shifting: true
+      shifting: true,
+      barStyle: {
+        backgroundColor: Colors.primaryColor
+      }
     })
     : createBottomTabNavigator(
         tabScreenConfig,
